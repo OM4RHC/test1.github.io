a584a34c33c0510d05bc6b4c6b94b6b338d50c39
OM4RHC <91348426+OM4RHC@users.noreply.github.com>
2021-09-25 18:45:07 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+﻿const yesbtn = document.querySelector("#yesbtn");
+
+yesbtn.addEventListener("click",function() {
+
+    alert("Ya sabía que querías jiji");
+
+})
+
+
+const notbtn = document.querySelector("#notbtn");
+
+notbtn.addEventListener("mouseover",function() {
+
+   const randomX = parseInt (Math.random()*100);
+
+   const randomY = parseInt (Math.random()*100);
+
+   notbtn.style.setProperty("top",randomY +"%");
+
+   notbtn.style.setProperty("left",randomX +"%");
+
+   notbtn.style.setProperty("transform","translate(-${randomX}%,-${randomY}%)");
+   
+})
